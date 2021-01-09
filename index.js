a8fb00950af79f81d19bf5db63525df15a26dec8
Evaldo Santos <jesantosjr@protonmail.com>
2021-01-08 23:15:25 -0300

    list tasklists

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 import { GraphQLServer } from "graphql-yoga";
-// ... or using `require()`
-// const { GraphQLServer } = require('graphql-yoga')
+import fetch, { Headers } from "node-fetch";
 
 const typeDefs = `
+  type TaskList {
+    kind: String,
+    id: String,
+    etag: String,
+    title: String,
+    updated: String,
+    selfLink: String
+  }
+
   type Query {
-    hello(name: String): String!
+    taskLists: [TaskList]
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: (_, { name }) => `Hello ${name || "World"}`,
+    taskLists: async (_, { name }, context) => {
+      const headers = new Headers({
+        "Content-Type": "application/json",
+        Authorization: context.token,
+      });
+
+      const options = {
+        headers,
+      };
+
+      return fetch(
+        "https://tasks.googleapis.com/tasks/v1/users/@me/lists",
+        options
+      )
+        .then((response) => response.json())
+        .then((response) => response.items);
+    },
   },
 };
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+  context: ({ request }) => {
+    const token = request.headers.authorization;
+
+    return {
+      ...request,
+      token,
+    };
+  },
+});
 server.start(() => console.log("Server is running on localhost:4000"));
